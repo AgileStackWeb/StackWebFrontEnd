37bd4d05c1886c2ffefb4378bfb54a27e7e69d7c
JimmyLu <luu21@kimo.com>
2023-06-02 01:44:19 +0800

    開始訓練頁面以及表格暫時完成

--- src/pages/StartTrainPage.vue
+++ src/pages/StartTrainPage.vue
@@ -1,26 +1,58 @@
 <template>
-  <div id="app" class="container" style="text-align: center">
-    <h5 class="card-title">所有資料的設定</h5>
-    <table style="align-content: center">
+  <div class="p-4">
+    <table class="min-w-full bg-white border border-gray-300">
       <thead>
         <tr>
-          <th>ID</th>
-          <th>名稱</th>
-          <th>價錢</th>
+          <th class="py-2 px-4 border-b" style="background-color:#E9E4F8; color: #8D7FB1;">Name</th>
+          <th class="py-2 px-4 border-b" style="background-color:#E9E4F8; color: #8D7FB1;">Age</th>
+          <th class="py-2 px-4 border-b" style="background-color:#E9E4F8; color: #8D7FB1;">Location</th>
         </tr>
       </thead>
       <tbody>
-        <TableRow
-          v-for="item in tableData"
-          :key="item.name"
-          :data="item"
-        ></TableRow>
+        <tr v-for="item in items" :key="item" class="hover:bg-gray-100">
+          <td class="py-2 px-4 border-b" style="color: #5A4888">
+            {{ item.player }}
+          </td>
+          <td class="py-2 px-4 border-b" style="color: #5A4888">
+            {{ item.age }}
+          </td>
+          <td class="py-2 px-4 border-b" style="color: #5A4888">
+            {{ item.country }}
+          </td>
+        </tr>
       </tbody>
     </table>
+    <TrainingButton></TrainingButton>
   </div>
 </template>
 
 <script setup>
+import TrainingButton from "../components/TrainingButton.vue";
 import { ref } from "vue";
-import Table from "../components/Table.vue";
+const items = ref([
+  {
+    player: "Stephen Curry",
+    age: 35,
+    country: "USA",
+  },
+  {
+    player: "Lebron James",
+    age: 38,
+    country: "USA",
+  },
+  {
+    player: "Kevin Durant",
+    age: 34,
+    country: "USA",
+  },
+  {
+    player: "Giannis Antetokounmpo",
+    age: 28,
+    country: "Greece",
+  },
+]);
 </script>
+
+<style>
+/* 添加Tailwind CSS的類名或自定義樣式 */
+</style>
