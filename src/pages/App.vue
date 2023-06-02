d6b6be104cad0a1707dd0daea6dd3a78146c315b
minnn311 <d0845235@0365.fcu.edu.tw>
2023-06-02 15:19:02 +0800

    把所有設定的把所有設定的table寫好版面及預設值

--- src/pages/App.vue
+++ src/pages/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="w-screen" style="background-color: #7A63B7;">
+  <nav class="w-screen" style="background-color: #7a63b7">
     <div class="max-w-7xl px-auto px-4 sm:px-6 lg:px-8">
       <div class="flex justify-between h-16">
         <div class="flex items-center">
--- src/pages/StartTrainPage.vue
+++ src/pages/StartTrainPage.vue
@@ -3,21 +3,72 @@
     <table class="min-w-full bg-white border border-gray-300">
       <thead>
         <tr>
-          <th class="py-2 px-4 border-b" style="background-color:#E9E4F8; color: #8D7FB1;">Name</th>
-          <th class="py-2 px-4 border-b" style="background-color:#E9E4F8; color: #8D7FB1;">Age</th>
-          <th class="py-2 px-4 border-b" style="background-color:#E9E4F8; color: #8D7FB1;">Location</th>
+          <th
+            class="py-2 px-4 border-b"
+            style="background-color: #e9e4f8; color: #8d7fb1"
+          >
+            模型名稱
+          </th>
+          <th
+            class="py-2 px-4 border-b"
+            style="background-color: #e9e4f8; color: #8d7fb1"
+          >
+            股票號碼
+          </th>
+          <th
+            class="py-2 px-4 border-b"
+            style="background-color: #e9e4f8; color: #8d7fb1"
+          >
+            公司名稱
+          </th>
+          <th
+            class="py-2 px-4 border-b"
+            style="background-color: #e9e4f8; color: #8d7fb1"
+          >
+            選取時間區間
+          </th>
+          <th
+            class="py-2 px-4 border-b"
+            style="background-color: #e9e4f8; color: #8d7fb1"
+          >
+            選擇模型：預設XGB
+          </th>
+          <th
+            class="py-2 px-4 border-b"
+            style="background-color: #e9e4f8; color: #8d7fb1"
+          >
+            選擇資料清洗： 預設Standard
+          </th>
+          <th
+            class="py-2 px-4 border-b"
+            style="background-color: #e9e4f8; color: #8d7fb1"
+          >
+          選擇技術指標：預設 純量價
+          </th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="item in items" :key="item" class="hover:bg-gray-100">
-          <td class="py-2 px-4 border-b" style="color: #5A4888">
-            {{ item.player }}
+          <td class="py-2 px-4 border-b" style="color: #5a4888">
+            {{ item.modelName }}
           </td>
-          <td class="py-2 px-4 border-b" style="color: #5A4888">
-            {{ item.age }}
+          <td class="py-2 px-4 border-b" style="color: #5a4888">
+            {{ item.stockNumber }}
           </td>
-          <td class="py-2 px-4 border-b" style="color: #5A4888">
-            {{ item.country }}
+          <td class="py-2 px-4 border-b" style="color: #5a4888">
+            {{ item.companyName }}
+          </td>
+          <td class="py-2 px-4 border-b" style="color: #5a4888">
+            {{ item.dateRange }}
+          </td>
+          <td class="py-2 px-4 border-b" style="color: #5a4888">
+            {{ item.chooseModel }}
+          </td>
+          <td class="py-2 px-4 border-b" style="color: #5a4888">
+            {{ item.dataCleaning }}
+          </td>
+          <td class="py-2 px-4 border-b" style="color: #5a4888">
+            {{ item.technicalIndicators }}
           </td>
         </tr>
       </tbody>
@@ -31,24 +82,40 @@ import TrainingButton from "../components/TrainingButton.vue";
 import { ref } from "vue";
 const items = ref([
   {
-    player: "Stephen Curry",
-    age: 35,
-    country: "USA",
+    modelName: "克林姆",
+    stockNumber: "0201",
+    companyName: '克林姆股份有限公司',
+    dateRange: '2023/02/11~2023/04/12',
+    chooseModel: 'XGB',
+    dataCleaning: 'Standard',
+    technicalIndicators: '純量價',
   },
   {
-    player: "Lebron James",
-    age: 38,
-    country: "USA",
+    modelName: "牧牧",
+    stockNumber: "0313",
+    companyName: '牧牧豬式會社',
+    dateRange: '2022/12/13~2023/01/05',
+    chooseModel: 'XGB',
+    dataCleaning: 'Standard',
+    technicalIndicators: '純量價',
   },
   {
-    player: "Kevin Durant",
-    age: 34,
-    country: "USA",
+    modelName: "Meiko",
+    stockNumber: "1231",
+    companyName: 'Meiko Inc',
+    dateRange: '2019/12/31~2020/01/10',
+    chooseModel: 'XGB',
+    dataCleaning: 'Standard',
+    technicalIndicators: '純量價',
   },
   {
-    player: "Giannis Antetokounmpo",
-    age: 28,
-    country: "Greece",
+    modelName: "芝麻糊",
+    stockNumber: "0113",
+    companyName: '芝麻糊公司',
+    dateRange: '2023/03/11~2023/11/12',
+    chooseModel: 'XGB',
+    dataCleaning: 'Standard',
+    technicalIndicators: '純量價',
   },
 ]);
 </script>
