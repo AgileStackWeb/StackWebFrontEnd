6e269242f2a44943a19c2dac65afe260bbb3de3d
fcu-d0843704 <yuxi512032@gmail.com>
2023-06-03 22:09:46 +0800

    新增模型清單頁面、回測頁面

---
+++ src/pages/ModelListPage.vue
@@ -0,0 +1,76 @@
+<template>
+<div class="p-4">
+    <table class="min-w-full bg-white border border-gray-300">
+        <thead>
+            <tr>
+                <th class="py-2 px-4 border-b" style="background-color: #e9e4f8; color: #8d7fb1">按鈕</th>
+                <th v-for="column in columns" :key="column.id" class="py-2 px-4 border-b" style="background-color: #e9e4f8; color: #8d7fb1">{{ column.label }}</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="row in rows" :key="row.id" class="hover:bg-gray-100">
+                <td :key="row.btn" class="py-2 px-4 border-b" style="color: #5a4888">
+                    <router-link class="text-purple hover:text-blue px-3 py-2 rounded-md text-sm font-medium" to="/predict">
+                        <font-awesome-icon @click="usemodel" class="icon" :icon="['fas', 'magnifying-glass']" />
+                    </router-link>
+                </td>
+                <td v-for="column in columns" :key="column.id" class="py-2 px-4 border-b" style="color: #5a4888">{{ row[column.field] }}</td>
+            </tr>
+        </tbody>
+    </table>
+    <TrainingButton></TrainingButton>
+</div>
+</template>
+
+<script setup>
+import TrainingButton from "../components/TrainingButton.vue";
+import {
+    ref
+} from "vue";
+
+function usemodel() {
+    // alert('usemodel.')
+    alert('usemodel.')
+    return data
+}
+const columns = ref([
+
+    {
+        id: 1,
+        label: '模型編號',
+        field: 'id'
+    },
+    {
+        id: 2,
+        label: '模型名稱',
+        field: 'name'
+    },
+    {
+        id: 3,
+        label: '時間區間',
+        field: 'time'
+    },
+    {
+        id: 4,
+        label: '技術指標',
+        field: 'tech'
+    }
+])
+const rows = ref([{
+        id: 0,
+        name: 'Model One',
+        time: '2023-04-01 ~ 2023-04-02',
+        tech: 'Standard'
+    },
+    {
+        id: 1,
+        name: 'Model Two',
+        time: '2023-05-01 ~ 2023-05-02',
+        tech: 'Standard'
+    }
+])
+</script>
+
+<style>
+/* 添加Tailwind CSS的類名或自定義樣式 */
+</style>
---
+++ src/pages/Predict.vue
@@ -0,0 +1,35 @@
+<template>
+<div class="box-border p-7 bg-white">
+    <div class="relative h-fit w-fit m-5">
+        <label for="range">選擇區間 :</label>
+        <input id="range" class="box-border" type="date" v-model="startDate" />
+        <span class="p-3">~</span>
+        <input type="date" class="box-border" v-model="endDate" />
+    </div>
+    <button @click="produceData">
+        開始預測
+        <font-awesome-icon class="icon" :icon="['fas', 'magnifying-glass']" />
+    </button>
+</div>
+<div>
+    <br>
+    <h2>{{ startDate }} ~ {{ endDate }}</h2>
+    <h2>預測結果</h2>
+
+</div>
+</template>
+
+<script setup>
+
+import { ref } from "vue";
+const startDate = ref("");
+const endDate = ref("");
+function produceData(){
+    const data = {
+    start: startDate.value,
+    end: endDate.value,
+  };
+    console.log('Predict Data',data)
+    alert('訓練成功!')
+}
+</script>
